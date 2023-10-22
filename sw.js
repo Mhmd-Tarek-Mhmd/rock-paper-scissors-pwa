e9e56964661d828b86d117c3a59beb24ed0fc54f
Mhmd-Tarek-Mhmd <mhmd.tarek197@gmail.com>
2023-10-22 07:09:58 +0300

    ADD: service worker

---
+++ sw.js
@@ -0,0 +1,16 @@
+const cacheName = "rock-paper-scissors-pwa";
+const filesToCache = ["index.html"];
+
+self.addEventListener("install", (e) =>
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(filesToCache))
+  )
+);
+
+self.addEventListener("fetch", (e) =>
+  e.respondWith(
+    caches
+      .match(e.request)
+      .then((cachedResponse) => cachedResponse || fetch(e.request))
+  )
+);
